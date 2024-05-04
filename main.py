6bd3f2883ab6e5df17dd287d976ad17e48d180f9
Christopher Méndez <mcmchris@hotmail.com>
2024-05-04 16:33:43 -0400

    update

--- main.py
+++ main.py
@@ -152,9 +152,9 @@ def capture(queueIn):
         #cropped_img = frame[0:720, 280:280+720]
         #resized_img = cv2.resize(frame, resize_dim, interpolation = cv2.INTER_AREA)
         
-        resized_img = cv2.resize(grey, resize_dim)
+        #resized_img = cv2.resize(grey, resize_dim)
         #img = cv2.cvtColor(resized_img, cv2.COLOR_BGR2RGB)
-        input_data = np.expand_dims(resized_img, axis=0)
+        input_data = np.expand_dims(grey, axis=0)
         if not queueIn.full():
             queueIn.put((grey, input_data))
 
